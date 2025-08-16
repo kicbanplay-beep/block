672e7506f351070b9927920672f71484f5b8a1f3
kicbanplay-beep <kicbanplay@gmail.com>
2025-08-17 04:23:45 +0800

    Update server.py

--- server.py
+++ server.py
@@ -1,24 +1,23 @@
 import asyncio
 import websockets
+import os
 
+PORT = int(os.environ.get("PORT", 8765))
 clients = set()
 
 async def handler(websocket, path):
     clients.add(websocket)
     try:
         async for message in websocket:
-            # рассылаем всем
             for client in clients:
                 if client != websocket:
                     await client.send(message)
-    except:
-        pass
     finally:
         clients.remove(websocket)
 
 async def main():
-    async with websockets.serve(handler, "0.0.0.0", 8765):
-        print("✅ WebSocket server запущен на ws://localhost:8765")
-        await asyncio.Future()  # вечный цикл
+    async with websockets.serve(handler, "0.0.0.0", PORT):
+        print(f"✅ WebSocket server запущен на порту {PORT}")
+        await asyncio.Future()
 
 asyncio.run(main())
